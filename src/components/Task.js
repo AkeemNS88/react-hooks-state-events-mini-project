427fcaaeb7d8cb75b637d8401ad34f8d44c4183a
Akeem <AkeemS11788@gmail.com>
2021-11-30 12:51:37 -0500

    commit tree 4e4e7531ba79a8b54b2901e9b4953e0ba9e1c4d9

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({task, handleDelete}) {
+  console.log(task)
+    // function handleClick(event){
+    //   // event.target.parentElement.remove()
+    // }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button onClick={() => handleDelete(task.text)} className="delete">X</button>
     </div>
   );
 }
